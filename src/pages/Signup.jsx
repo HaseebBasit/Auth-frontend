e2c5fed53adc6cf94bd9e43b25a1ec0703f16fe2
HaseebBasit <haseebbasit2707@gmail.com>
2026-09-17 23:41:58 +0500

    minor changes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -29,8 +29,8 @@ export default function Signup() {
       setError('All fields are required');
       return;
     }
-    if (form.password.length < 6) {
-      setError('Password must be at least 6 characters');
+    if (form.password.length < 8) {
+      setError('Password must be at least 8 characters');
       return;
     }
     if (form.password !== form.confirmPassword) {
